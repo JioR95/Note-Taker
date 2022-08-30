c68930f501bb399fcace1203bc62fd6f249a3f40
JioR95 <jiovani.rivera@rocketmail.com>
2022-08-30 19:58:52 -0400

    notes.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
-const fs = require('fs');
+
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
