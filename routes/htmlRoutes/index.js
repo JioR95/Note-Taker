86adc4642d3f95c2649f354398750ad4f3260d31
JioR95 <jiovani.rivera@rocketmail.com>
2022-08-30 19:10:53 -0400

    htmlRoutes index.js finish

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -2,10 +2,15 @@ const path = require('path');
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    res.sendFile(path.joint(_dirname, '../../public/index.html'));
+    res.sendFile(path.joint(__dirname, '../../public/index.html'));
 });
 
 router.get('/notes', (req, res) => {
-    res.sendFile(path.joint( _dirname, '../../public/index.html'));
+    res.sendFile(path.joint(__dirname, '../../public/index.html'));
 });
 
+router.get('*', (req, res) => {
+    res.sendFiles(path.joint(__dirname, '../../public/index.html'));
+});
+
+module.exports = router;
