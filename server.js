fb9361611a6b9943cd7ef490f16bc9911eea1631
JioR95 <jiovani.rivera@rocketmail.com>
2022-09-01 17:50:03 -0400

    check update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
-
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
@@ -12,6 +12,16 @@ app.use(express.json());
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
-app.listen(PORT, () => {
-    console.log('Api server now on port ${PORT}!');
-});
+app.listen(PORT, function () {
+    console.log(`The SERVER HAS STARTED ON PORT: ${PORT}`);
+  })
+  //   Fix the Error EADDRINUSE
+  .on("error", function (err) {
+    process.once("SIGUSR2", function () {
+      process.kill(process.pid, "SIGUSR2");
+    });
+    process.on("SIGINT", function () {
+      // this is only called on ctrl+c, not restart
+      process.kill(process.pid, "SIGINT");
+    });
+  });
