d5089e108fdd9d025740c54bf8ec506cdf5fc705
JioR95 <jiovani.rivera@rocketmail.com>
2022-08-31 20:43:57 -0400

    fix error

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -6,7 +6,7 @@ router.get('/', (req, res) => {
 });
 
 router.get('/notes', (req, res) => {
-    res.sendFile(path.joint(__dirname, '../../public/index.html'));
+    res.sendFile(path.joint(__dirname, '../../public/notes.html'));
 });
 
 router.get('*', (req, res) => {
